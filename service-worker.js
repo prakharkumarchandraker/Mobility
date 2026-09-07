3cdc9945755ffc62899685434d2d809aaf3b73d9
Prakhar Kumar Chandraker <prakharkumarchandraker@gmail.com>
2026-09-07 17:34:58 +0530

    new

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'lifeos-v1';
+const CACHE = 'lifeos-v4';
 const ASSETS = ['./index.html', './life-os.html', './manifest.json', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   self.skipWaiting();
